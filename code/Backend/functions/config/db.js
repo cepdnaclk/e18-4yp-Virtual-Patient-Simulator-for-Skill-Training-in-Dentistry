0a1106b66bf56c145f348e0242721533157df5d9
Dilandds <e17379@eng.pdn.ac.lk>
2025-10-03 15:22:57 +0530

    bucket name change

--- code/Backend/functions/config/db.js
+++ code/Backend/functions/config/db.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-  storageBucket: process.env.FIREBASE_BUCKET || `${process.env.GCLOUD_PROJECT}.appspot.com`,
+  storageBucket: process.env.FIREBASE_BUCKET || `${process.env.GCLOUD_PROJECT}.firebasestorage.app`,
 });
 
 const db = admin.firestore();
